56c3f180e8ba2e57bbe836e06c80339b2379aa00
Saad <saad_anwer@live.com>
2019-12-27 21:15:01 +0500

    bottomsheet done

--- app/src/main/java/com/example/sathchaloodriver/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sathchaloodriver/ui/home/HomeFragment.kt
@@ -4,28 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.sathchaloodriver.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
-
-    private lateinit var homeViewModel: HomeViewModel
+private var btn:Button?= null
+//    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+//        homeViewModel =
+//            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+btn = root.findViewById(R.id.button)
+        btn!!.setOnClickListener {
+            val view = layoutInflater.inflate(R.layout.activity_pickup_bottomsheet, null)
+            val dialog = BottomSheetDialog(root.context)
+            dialog.setContentView(view)
+            dialog.show()
+        }
+
         return root
     }
 }
